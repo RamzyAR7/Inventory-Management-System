9d9f8d8981d9f2eaf5a3c04a882d4f3c63c3b0a7
Noaman <mn0359948@gmail.com>
2025-04-08 00:47:10 +0200

    Create Category Controller

---
+++ Inventory-Management-System/ViewModels/OrderViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Inventory_Management_System.ViewModels
+{
+    public class OrderViewModel
+    {
+        public int OrderID { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        public Guid CustomerID { get; set; }
+        public IEnumerable<SelectListItem> Customers { get; set; }
+
+        public Guid WarehouseID { get; set; }
+        public IEnumerable<SelectListItem> Warehouses { get; set; }
+
+        public string Notes { get; set; }
+    }
+
+}
