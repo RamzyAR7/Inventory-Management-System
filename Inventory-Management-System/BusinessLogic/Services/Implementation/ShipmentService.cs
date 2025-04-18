a809a73c20d2ca6f5793f2a877094822b8a40db4
Noaman <mn0359948@gmail.com>
2025-04-19 01:23:25 +0200

    Combine some views in back end

---
+++ Inventory-Management-System/BusinessLogic/Services/Implementation/ShipmentService.cs
@@ -0,0 +1,55 @@
+﻿using Inventory_Management_System.BusinessLogic.Services.Interface;
+using Inventory_Management_System.DataAccess.Context;
+using Inventory_Management_System.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Inventory_Management_System.BusinessLogic.Services.Implementation
+{
+    public class ShipmentService : IShipmentService
+    {
+        private readonly InventoryDbContext _context;
+
+        public ShipmentService(InventoryDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Shipment>> GetAllAsync()
+        {
+            return await _context.Shipments
+                .Include(s => s.Warehouse)
+                .Include(s => s.Order)
+                .ToListAsync();
+        }
+
+        public async Task<Shipment?> GetByIdAsync(Guid id)
+        {
+            return await _context.Shipments
+                .Include(s => s.Warehouse)
+                .Include(s => s.Order)
+                .FirstOrDefaultAsync(s => s.ShipmentID == id);
+        }
+
+        public async Task CreateAsync(Shipment shipment)
+        {
+            _context.Shipments.Add(shipment);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Shipment shipment)
+        {
+            _context.Shipments.Update(shipment);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var shipment = await _context.Shipments.FindAsync(id);
+            if (shipment != null)
+            {
+                _context.Shipments.Remove(shipment);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
--- Inventory-Management-System/Controllers/ShipmentsController.cs
+++ Inventory-Management-System/Controllers/ShipmentsController.cs
@@ -1,12 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Inventory_Management_System.BusinessLogic.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Inventory_Management_System.Controllers
 {
     public class ShipmentsController : Controller
     {
-        public IActionResult Index()
+        private readonly IShipmentService _shipmentService;
+
+        public ShipmentsController(IShipmentService shipmentService)
         {
-            return View();
+            _shipmentService = shipmentService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var shipments = await _shipmentService.GetAllAsync();
+            return View(shipments);
         }
     }
 }
