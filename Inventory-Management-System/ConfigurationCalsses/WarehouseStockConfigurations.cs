2667b0ca016121d967c8baba534d39628fba8d12
Noaman <mn0359948@gmail.com>
2025-04-30 22:54:58 +0300

    AddConfigToWarehouseStock

---
+++ Inventory-Management-System/ConfigurationCalsses/WarehouseStockConfigurations.cs
@@ -0,0 +1,25 @@
+﻿using Inventory_Management_System.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Inventory_Management_System.ConfigurationCalsses
+{
+    public class WarehouseConfigurations : IEntityTypeConfiguration<Warehouse>
+    {
+        public void Configure(EntityTypeBuilder<Warehouse> builder)
+        {
+            builder.HasKey(w => w.WarehouseID);
+            builder.Property(w => w.Address)
+            .HasMaxLength(255);
+            builder.HasOne(w => w.Manager)
+            .WithMany(u => u.ManagedWarehouses)
+            .HasForeignKey(w => w.ManagerID);
+
+            builder.HasIndex(w => w.WarehouseName).IsUnique();
+
+        }
+
+
+    }
+    
+}
