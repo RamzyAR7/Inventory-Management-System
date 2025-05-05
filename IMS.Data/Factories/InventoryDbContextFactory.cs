100bc7b26606b9e08341fa4f501fd5aa21456472
Noaman <mn0359948@gmail.com>
2025-05-05 22:55:55 +0300

    Edit DBContext

---
+++ IMS.Data/Factories/InventoryDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using IMS.DAL.Context;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.DAL.Factories
+{
+    public class InventoryDbContextFactory : IDesignTimeDbContextFactory<InventoryDbContext>
+    {
+        public InventoryDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), "../IMS.Web"))
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<InventoryDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseSqlServer(connectionString);
+
+            return new InventoryDbContext(builder.Options);
+        }
+    }
+}
