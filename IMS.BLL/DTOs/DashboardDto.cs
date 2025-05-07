5c8578657337c28284cf4e29e34165c7bdb86a19
Noaman <mn0359948@gmail.com>
2025-05-07 19:43:22 +0300

    EditDashboard

--- IMS.BLL/DTOs/DashboardDto.cs
+++ IMS.BLL/DTOs/DashboardDto.cs
@@ -14,9 +14,16 @@
         public int TotalInventoryTransactions { get; set; }
         public int TotalSuppliedProducts { get; set; }
         public int CompletedOrders { get; set; }
-
         public string TopSellingProductName { get; set; }
         public int TopSellingProductQuantity { get; set; }
+
+        public int InTransactions { get; set; }
+        public int OutTransactions { get; set; }
+        public int WarehouseTransfers { get; set; }
+
+        public int LowStockItems { get; set; }
+        public int PendingOrders { get; set; }
+        public int RecentTransactions { get; set; } // Transactions in the last 7 days
     }
 
 }
